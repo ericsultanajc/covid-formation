f3807a252d120b7a95e408d8e678325a00cd570e
Damien <dubreuil.damien@laposte.net>
2020-04-28 11:21:56 +0200

    Imports

--- sopra-formation-model/src/main/java/sopra/formation/model/Formateur.java
+++ sopra-formation-model/src/main/java/sopra/formation/model/Formateur.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
