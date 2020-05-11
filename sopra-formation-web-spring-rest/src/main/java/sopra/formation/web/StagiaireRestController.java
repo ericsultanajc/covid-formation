c381e68163b43659eb74d9dac8cc38097b5371c9
Hache <lucas.hache70@gmail.com>
2020-05-11 15:31:47 +0200

    Auto stash before merge of "lucas" and "origin/master"

--- sopra-formation-web-spring-rest/src/main/java/sopra/formation/web/StagiaireRestController.java
+++ sopra-formation-web-spring-rest/src/main/java/sopra/formation/web/StagiaireRestController.java
@@ -1,59 +1,38 @@
 package sopra.formation.web;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.fasterxml.jackson.annotation.JsonView;
-
+import sopra.formation.model.Evaluation;
 import sopra.formation.model.Stagiaire;
-import sopra.formation.model.Views;
+import sopra.formation.persistence.IEvaluationRepository;
 import sopra.formation.persistence.IStagiaireRepository;
 
 @RestController
-@RequestMapping("/stagiaire")
 public class StagiaireRestController {
 
 	@Autowired
 	private IStagiaireRepository stagiaireRepo;
 
-	@GetMapping("")
-	@JsonView(Views.ViewStagiaire.class)
+	@GetMapping("/stagiaire")
 	public List<Stagiaire> findAll() {
 		return stagiaireRepo.findAll();
 	}
 
-	@GetMapping("/by-formateur/{nom}")
-	@JsonView(Views.ViewStagiaire.class)
-	public List<Stagiaire> findAllByFormateur(@PathVariable String nom) {
-		return stagiaireRepo.findAllByFormateur(nom);
-	}
-
-	@GetMapping("/by-ville/{ville}")
-	@JsonView(Views.ViewStagiaire.class)
-	public List<Stagiaire> findAllByVille(@PathVariable String ville) {
-		return stagiaireRepo.findAllByVille(ville);
-	}
-
-	@GetMapping("/by-nom/{nom}")
-	@JsonView(Views.ViewStagiaire.class)
-	public List<Stagiaire> findAllByNom(@PathVariable String nom) {
-		return stagiaireRepo.findAllByNom(nom + "%");
-	}
-
-	@GetMapping("/{id}")
-	@JsonView(Views.ViewStagiaire.class)
+	@GetMapping("/stagiaire/{id}")
 	public Stagiaire find(@PathVariable Long id) {
 
 		Optional<Stagiaire> optStagiaire = stagiaireRepo.findById(id);
@@ -64,28 +43,15 @@ public class StagiaireRestController {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
 		}
 	}
-	
-	@GetMapping("/{id}/detail")
-	@JsonView(Views.ViewStagiaireDetail.class)
-	public Stagiaire findDetail(@PathVariable Long id) {
 
-		Optional<Stagiaire> optStagiaire = stagiaireRepo.findById(id);
-
-		if (optStagiaire.isPresent()) {
-			return optStagiaire.get();
-		} else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
-		}
-	}
-
-	@PostMapping("")
+	@PostMapping("/stagiaire")
 	public Stagiaire create(@RequestBody Stagiaire stagiaire) {
 		stagiaire = stagiaireRepo.save(stagiaire);
 
 		return stagiaire;
 	}
 
-	@PutMapping("/{id}")
+	@PutMapping("/stagiaire/{id}")
 	public Stagiaire update(@RequestBody Stagiaire stagiaire, @PathVariable Long id) {
 		if (!stagiaireRepo.existsById(id)) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
@@ -96,8 +62,8 @@ public class StagiaireRestController {
 		return stagiaire;
 	}
 
-	@DeleteMapping("/{id}")
-	public void delete(@PathVariable Long id) {
+	@DeleteMapping("/stagiaire/{id}")
+	public void delete (@PathVariable Long id) {
 		stagiaireRepo.deleteById(id);
 	}
 }
